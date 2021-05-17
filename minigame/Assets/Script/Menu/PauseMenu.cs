13308699487b018dbb714277cefa1abee7cff8ba
YuxuanChen1 <2580069953@qq.com>
2021-05-17 12:01:08 +0800

    暂停

---
+++ minigame/Assets/Script/Menu/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pause;
+    [SerializeField] GameObject pauseButton;
+    // Start is called before the first frame update
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        pause.SetActive(true);
+        pauseButton.SetActive(false);
+    }
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadSceneAsync("主菜单");
+        Time.timeScale = 1;
+    }
+
+    public void RePlay()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+
+    public void Continue()
+    {
+        Time.timeScale = 1;
+        pause.SetActive(false);
+        pauseButton.SetActive(true);
+    }
+}
--- minigame/Assets/Script/画线区域/OneWayPlatform_DrawArea.cs
+++ minigame/Assets/Script/画线区域/OneWayPlatform_DrawArea.cs
@@ -7,10 +7,17 @@ public class OneWayPlatform_DrawArea : MonoBehaviour
     [SerializeField] public float size;
     [SerializeField] private Transform left;
     [SerializeField] private Transform right;
+    [SerializeField] private GameObject pencil;
+    [SerializeField] private Collider2D _collider;
 
     private void Awake()
     {
         size = right.position.x - left.position.x;
     }
-    
+
+    private void Start()
+    {
+        pencil = GameObject.FindGameObjectWithTag("Pencil");
+        Physics2D.IgnoreCollision(_collider, pencil.GetComponent<Collider2D>());
+    }
 }
